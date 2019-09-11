b6d68fce9188799284640f4e0e3a33d5fdf8d71d
nicolasdbasso <52472551+nicolasdbasso@users.noreply.github.com>
2019-09-11 18:17:31 -0300

    Add files via upload

---
+++ calculos.h
@@ -0,0 +1,11 @@
+/*
+ * calculos.h
+ *
+ *  Created on: 6 sep. 2019
+ *      Author: nicolas
+ */
+
+int sumaDeDosInt (int numero1, int numero2, int* resultado);
+int restaDeDosInt (int numero1, int numero2, int* resultado);
+int multiplicacionDeDosInt (int numero1, int numero2, int* resultado);
+int divisionDeDosInt (int numero1, int numero2, float* resultado);
